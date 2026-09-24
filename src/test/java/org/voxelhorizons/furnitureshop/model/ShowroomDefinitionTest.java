b51939759d94002a98ad4c0af99e7cd5a22ac298
Dan Alexander <71574715+Fibbicles@users.noreply.github.com>
2026-09-24 17:24:50 +1000

    Test persistent showroom furniture groups

--- src/test/java/org/voxelhorizons/furnitureshop/model/ShowroomDefinitionTest.java
+++ src/test/java/org/voxelhorizons/furnitureshop/model/ShowroomDefinitionTest.java
@@ -10,9 +10,12 @@ public class ShowroomDefinitionTest {
         ShowroomDefinition showroom = new ShowroomDefinition();
         ShopCuboid region = new ShopCuboid("world", 0, 0, 0, 5, 5, 5);
         showroom.region("seasonal_upstairs", region);
+        ShopCuboid curtains = new ShopCuboid("world", 1, 1, 1, 3, 3, 1);
+        showroom.furnitureGroup("front_curtains", curtains);
         showroom.activeVariant("seasonal_upstairs", "autumn_setup");
         showroom.require("seasonal_upstairs", "autumn_setup", "main", "autumn_railing");
         assertEquals(region, showroom.regions().get("seasonal_upstairs"));
+        assertEquals(curtains, showroom.furnitureGroups().get("front_curtains"));
         assertEquals("autumn_setup", showroom.activeVariants().get("seasonal_upstairs"));
         assertEquals("autumn_railing", showroom.requirements("seasonal_upstairs", "autumn_setup").get("main"));
         assertNull(showroom.exit());
